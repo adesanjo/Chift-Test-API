233321982e050731dcd9c68d6035ca64fcff40c2
adesanjo <jesuisanonime@gmail.com>
2025-01-24 12:37:09 +0100

    Added pydantic response models

--- main.py
+++ main.py
@@ -1,51 +1,69 @@
 import sqlite3
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from constants import DATABASE
 
 app = FastAPI()
 
+class Contact(BaseModel):
+    id: int
+    name: str
+    email: str
+    phone: str
+
+class Invoice(BaseModel):
+    id: int
+    name: str
+    invoice_date: str
+    amount_total: float
+    state: str
+    partner_id: int
+
+class Error(BaseModel):
+    error: str
+
 @app.get("/contacts")
-async def contacts():
+async def contacts() -> list[Contact]:
     con = sqlite3.connect(DATABASE)
     cur = con.cursor()
     cur.execute("SELECT * FROM contacts")
     dbContacts = cur.fetchall()
     con.close()
-    response = [dict(zip(("id", "name", "email", "phone"), contact)) for contact in dbContacts]
+    response = [Contact(**dict(zip(("id", "name", "email", "phone"), contact))) for contact in dbContacts]
     return response
 
 @app.get("/contacts/{contact_id}")
-async def contact(contact_id: int):
+async def contact(contact_id: int) -> Contact | Error:
     con = sqlite3.connect(DATABASE)
     cur = con.cursor()
     cur.execute("SELECT * FROM contacts WHERE id = ?", (contact_id,))
     dbContact = cur.fetchone()
     con.close()
     if dbContact is None:
-        return {"error": "Contact not found"}
-    response = dict(zip(("id", "name", "email", "phone"), dbContact))
+        return Error(error="Contact not found")
+    response = Contact(**dict(zip(("id", "name", "email", "phone"), dbContact)))
     return response
 
 @app.get("/invoices")
-async def invoices():
+async def invoices() -> list[Invoice]:
     con = sqlite3.connect(DATABASE)
     cur = con.cursor()
     cur.execute("SELECT * FROM invoices")
     dbInvoices = cur.fetchall()
     con.close()
-    response = [dict(zip(("id", "name", "invoice_date", "amount_total", "state", "partner_id"), invoice)) for invoice in dbInvoices]
+    response = [Invoice(**dict(zip(("id", "name", "invoice_date", "amount_total", "state", "partner_id"), invoice))) for invoice in dbInvoices]
     return response
 
 @app.get("/invoices/{invoice_id}")
-async def invoice(invoice_id: int):
+async def invoice(invoice_id: int) -> Invoice | Error:
     con = sqlite3.connect(DATABASE)
     cur = con.cursor()
     cur.execute("SELECT * FROM invoices WHERE id = ?", (invoice_id,))
     dbInvoice = cur.fetchone()
     con.close()
     if dbInvoice is None:
-        return {"error": "Invoice not found"}
-    response = dict(zip(("id", "name", "invoice_date", "amount_total", "state", "partner_id"), dbInvoice))
+        return Error(error="Invoice not found")
+    response = Invoice(**dict(zip(("id", "name", "invoice_date", "amount_total", "state", "partner_id"), dbInvoice)))
     return response
